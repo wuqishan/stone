29e999ea50b7469ed5878040448029d84ccdccbc
wells <qishan.wu.1@lexisnexis.com>
2017-08-30 17:38:58 +0800

    refs: 导航分类设置

---
+++ public/admin/datas/nav2.js
@@ -0,0 +1,26 @@
+var navs2 = [{
+	"title": "基本元素",
+	"icon": "fa-cubes",
+	"spread": false,
+	"children": [{
+		"title": "按钮",
+		"icon": "&#xe641;",
+		"href": "button.html"
+	}, {
+		"title": "表单",
+		"icon": "&#xe63c;",
+		"href": "/admin/image/create"
+	}, {
+		"title": "表格",
+		"icon": "&#xe63c;",
+		"href": "table.html"
+	}, {
+		"title": "导航",
+		"icon": "&#xe609;",
+		"href": "nav.html"
+	}, {
+		"title": "辅助性元素",
+		"icon": "&#xe60c;",
+		"href": "auxiliar.html"
+	}]
+}];
--- resources/views/admin/common/header.blade.php
+++ resources/views/admin/common/header.blade.php
@@ -16,6 +16,12 @@
         </div>
         <ul class="layui-nav admin-header-item" id="menu">
             <li class="layui-nav-item">
+                <a href="javascript:;" module-id="0">内容管理</a>
+            </li>
+            <li class="layui-nav-item">
+                <a href="javascript:;" module-id="1">网站管理</a>
+            </li>
+            <li class="layui-nav-item">
                 <a href="javascript:;">清除缓存</a>
             </li>
             <li class="layui-nav-item">
